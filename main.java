4bf26ff1a8b4c88cb929f4f0401cfe932e3d37ee
IamNeekro <alernbgd@gmail.com>
2023-12-20 22:43:53 -0300

    calculadora limpa

--- main.java
+++ main.java
@@ -4,25 +4,6 @@ import java.util.concurrent.TimeUnit;
 public class main {
 
 public static void main(String[] args){
-
-// final int cons = 1; //constante 
-
-
-// int[][] matriz = new int[4][4]; // matriz
-// for(var valor = 0; valor < 4; valor++ ){
-//         // System.out.printf("\n linha da matriz numero %d", valor);
-//     for(var valor1 = 0; valor1 < 4; valor1++){
-//     // System.out.printf("\n coluna da matriz numero %d", valor1);
-//     matriz[valor][valor1] = valor1;
-//     }
-// } //laço de repetição
-
-// for(int[] n:matriz){ //foreach
-//     // System.out.printf("Linha: %d",n);
-//     for(int f:n){
-//         System.out.printf("\nLinha: " + n + "\n Coluna: " + f);
-//     }
-// }
 while (true) {
 Scanner scan = new Scanner(System.in);
 System.out.println("digite o primeiro valor do cálculo: ");
