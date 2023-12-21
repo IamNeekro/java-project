ba8458e6a78f49b0ab1eed1e4a61eff704afa54e
IamNeekro <alernbgd@gmail.com>
2023-12-20 22:39:33 -0300

    calculadora simples

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;  // Import the Scanner class
-
+import java.util.concurrent.TimeUnit;
 
 public class main {
 
@@ -23,12 +23,13 @@ public static void main(String[] args){
 //         System.out.printf("\nLinha: " + n + "\n Coluna: " + f);
 //     }
 // }
+while (true) {
 Scanner scan = new Scanner(System.in);
-System.out.println("digite o primeiro valor do cálculo");
+System.out.println("digite o primeiro valor do cálculo: ");
 var valor1 = scan.nextInt();
-System.out.println("digite o sinal");
-var valor2 = scan.nextLine();
-System.out.println("digite o segundo valor do cálculo");
+System.out.println("digite o sinal: ");
+var valor2 = scan.next();
+System.out.println("digite o segundo valor do cálculo: ");
 var valor3 = scan.nextInt();
 if(valor2.equals("/")){
 var total = (valor1 / valor3);
@@ -38,7 +39,30 @@ if(valor2.equals("*")){
    var total = (valor1 * valor3);
 System.out.printf("o valor do seu cálculo é de %d", total); 
 }
+if(valor2.equals("+")){
+   var total = (valor1 + valor3);
+System.out.printf("o valor do seu cálculo é de %d", total); 
+}
+if(valor2.equals("-")){
+   var total = (valor1 - valor3);
+System.out.printf("o valor do seu cálculo é de %d", total); 
+}
+System.out.println("\ncálculo finalizado");
 
-
+try{
+Thread.sleep(200);
+}catch(InterruptedException e){
+    // Thread.currentThread().interrupt();
+}
+System.out.println("Continue? s/n");
+var continuar = scan.next();
+if(continuar.equals("s") || continuar.equals("S")){
+    continue;
+}else{
+    break;
 }
 }
+System.out.printf("programa finalizado!");
+}
+}
+//bagunça do carai
