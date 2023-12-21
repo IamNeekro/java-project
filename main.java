1a8da9a640f14e258d6587d237e253bfe8ca620e
IamNeekro <alernbgd@gmail.com>
2023-12-20 21:38:40 -0300

    minimalist study

---
+++ main.java
@@ -0,0 +1,16 @@
+public class main {
+
+public static void main(String[] args){
+
+final int cons = 1; //constante 
+
+
+int[][] matriz = new int[4][4]; // matriz
+for(var valor = 0; valor < 4; valor++ ){
+    System.out.printf("\n linha da matriz numero %s", valor);
+    for(var valor1 = 0; valor1 < 4; valor1++){
+    System.out.printf("\n coluna da matriz numero %s", valor1);
+    }
+} //laço de repetição
+}
+}
